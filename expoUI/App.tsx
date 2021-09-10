58328df927d95e4d1a2370a0d07a6b505f64033c
kevinlimBU <kevinlim212@gmail.com>
2021-09-10 03:42:05 -0400

    add return and scan buttons

--- expoUI/App.tsx
+++ expoUI/App.tsx
@@ -1,8 +1,8 @@
-//miniProject UI. Access camera first try
-//written by Kevin Lim 
+//miniProject UI. Access camera  
+//author: Kevin Lim 
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Dimensions } from 'react-native';
 import {Camera} from 'expo-camera'
 
 
@@ -12,7 +12,7 @@ export default function App() {
   const [startCamera, setStartCamera] = React.useState(false)
 
 
-  const __startCamera = async () => {
+  const openCamera = async () => {
     const {status} = await Camera.requestPermissionsAsync()
  if(status === 'granted'){
   setStartCamera(true)
@@ -21,44 +21,62 @@ export default function App() {
  }
 }
 
+const closeCamera = async () => {
+  setStartCamera(false)
+}
+
   return (
 
-    startCamera?(<Camera
-      style={styles.cameraView}
-      ></Camera>
-    ):(<View style={styles.container}>
+    startCamera?(<Camera style={styles.cameraView}>
+        <View style={styles.cameraReturn}>
+            <Button title = 'Return' color='#ffffff' onPress={closeCamera}/>
+        </View>
+        <TouchableOpacity style={styles.scanButton} /*onPress={}*/ />
+      </Camera>
+    ):(
+
+    <View style={styles.container}>
       <Text>Welcome! Press Button to Scan</Text>
-
       <Text>  </Text>
-      <Text>  </Text>
-      <Text>  </Text>
-
       <View style={styles.buttonBox}>
-        <Button title = 'Start Scanning' onPress = {__startCamera}/>  
+        <Button title = 'Start Scanning' color='#ffffff' onPress = {openCamera}/>  
       </View>
 
       <StatusBar style = "auto" />
     </View>)
-    
 
-    
     )
   }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 
   buttonBox: {
-    marginTop: 20
+    marginTop: 20,
+    backgroundColor: '#1a53ff',
   },
-   cameraView: {
-     flex: 1,
-     width: "100%"
-   }
+  cameraView: {
+    flex: 1,
+    width: "100%"
+  },
+  cameraReturn: {
+    position: 'absolute',
+    top: 40,
+    left: 20
+  },
+  scanButton: {
+    position: 'absolute',
+    width: 80,
+    height: 80,
+    borderRadius: 50,
+    bottom: 40,
+    left: Dimensions.get('window').width/2 - 40,
+    backgroundColor: '#ffffff'
 
+  }
 })
