effa41ba021c31917c9d424427cf646e6aae940b
kevinlimBU <kevinlim212@gmail.com>
2021-09-13 05:50:58 -0400

    add firebase config for testing

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 //miniProject main entry point
-//author: Alex Necakov
+//author: Alex Necakov, Kevin Lim
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -8,6 +8,23 @@ import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import ScanScreen from './screens/ScanScreen';
 import UserScreen from './screens/User Screen';
+import * as firebase from 'firebase';
+//import firebase from 'firebase'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAJnt5z9yx3YBgrzBYti5BSMj2U2bDVHRI",
+    authDomain: "firetest-8ef93.firebaseapp.com",
+    projectId: "firetest-8ef93",
+    storageBucket: "firetest-8ef93.appspot.com",
+    messagingSenderId: "160298483133",
+    appId: "1:160298483133:web:dc6afe76d50a6d5641a6d4",
+    measurementId: "G-E912FNJMRH"
+  };
+
+/*if(firebase.apps.length == 0){
+    firebase.initializeApp(firebaseConfig)
+}*/
 
 export default function App() {
     const isLoadingComplete = useCachedResources();
