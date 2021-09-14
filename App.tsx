ab8d79aaf4841fe14818f5fd11d5337559034da7
kevinlimBU <kevinlim212@gmail.com>
2021-09-14 00:11:30 -0400

    connect to firebase first try

--- App.tsx
+++ App.tsx
@@ -9,9 +9,10 @@ import Navigation from './navigation';
 import ScanScreen from './screens/ScanScreen';
 import UserScreen from './screens/User Screen';
 import firebase from 'firebase'
+import { firebaseConfig } from './config';
 //import 'firebase/firestore'
 
-const firebaseConfig = {
+/*const firebaseConfig = {
     apiKey: "AIzaSyAJnt5z9yx3YBgrzBYti5BSMj2U2bDVHRI",
     authDomain: "firetest-8ef93.firebaseapp.com",
     projectId: "firetest-8ef93",
@@ -19,11 +20,13 @@ const firebaseConfig = {
     messagingSenderId: "160298483133",
     appId: "1:160298483133:web:dc6afe76d50a6d5641a6d4",
     measurementId: "G-E912FNJMRH"
-  };
+  };*/
 
 /*if(firebase.apps.length == 0){
     firebase.initializeApp(firebaseConfig)
 }*/
+
+firebase.initializeApp(firebaseConfig)
 
 export default function App() {
     const isLoadingComplete = useCachedResources();
