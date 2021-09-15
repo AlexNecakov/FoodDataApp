109bbbb16d67ccf2a5007033ec775616058079e5
AlexNecakov <alexnecakov@gmail.com>
2021-09-14 22:49:35 -0400

    successful api query and data retrieval

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,44 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { Platform, StyleSheet } from 'react-native';
 import ReactJson from 'react-json-view'
 import { Text, View } from '../components/Themed';
 import { RootStackScreenProps } from '../types';
+import axios from 'axios';
 
 export default function ModalScreen({ route, navigation }: RootStackScreenProps<'Modal'>) {
-
     const barCode: string = route.params.upcCode;
+    let [totalHits, setTotalHits] = useState(0);
+    let [foodDescription, setDescription] = useState('');
+
     const getFoodProfilefromFDA = async (barCode: string) => {
-        try {
-            //var url = 'https://api.nal.usda.gov/fdc/v1/foods/search?query=' + 'barCode' + '&api_key=IUK2OzgXgQx5a9rO0fAWPaUjd1LQf5sAh4q4jEsb'
-            //nutella test case
-            var url = 'https://api.nal.usda.gov/fdc/v1/foods/search?query=' + '009800800254' + '&api_key=IUK2OzgXgQx5a9rO0fAWPaUjd1LQf5sAh4q4jEsb'
-            const response = await fetch(url,
-                {
-                    method: 'POST',
-                    headers: {
-                        Accept: 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                    })
+        var url = 'https://api.nal.usda.gov/fdc/v1/foods/search?query=' + 'barCode' + '&api_key=IUK2OzgXgQx5a9rO0fAWPaUjd1LQf5sAh4q4jEsb'
+        //nutella test case
+        //var url = 'https://api.nal.usda.gov/fdc/v1/foods/search?query=' + '009800800254' + '&api_key=IUK2OzgXgQx5a9rO0fAWPaUjd1LQf5sAh4q4jEsb'
+        fetch(url, {
+            "method": "GET"
+        })
+            .then(response => response.json())
+            .then(response => {
+                setTotalHits(response.totalHits);
+                if (totalHits > 0) {
+                    setDescription(response.foods[0].description);
                 }
-            );
-            const json = await response.json();
-            const jsonFoods = json?.foods ? json.foods : 'No foods found';
-            if (jsonFoods.length > 0) {
-                return jsonFoods[0]?.description ? jsonFoods[0].description : 'No name in database';
-            } else {
-                return 'No foods found';
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
+                else {
+                    setDescription('Food not found in database');
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+    useEffect(() => {
+        getFoodProfilefromFDA(barCode);
+    }, [])
+
     return (
 
         <View style={styles.container}>
-            <Text style={styles.title}>{barCode}</Text>
+            <Text style={styles.title}> {foodDescription}</Text>
             <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
 
             {/* Use a light status bar on iOS to account for the black space above the modal */}
