401786f3fe880f9054c0a838dff77135fd366f43
kevinlimBU <kevinlim212@gmail.com>
2021-09-15 02:33:25 -0400

    attempt to fetch data from firestore to userscreen

--- screens/User Screen.tsx
+++ screens/User Screen.tsx
@@ -1,38 +1,57 @@
 //import * as React from 'react';
 import React, {FC, useState, useEffect} from 'react';
-import { StyleSheet, Button} from 'react-native';
+import { Text, View, StyleSheet, Button, TouchableOpacity} from 'react-native';
 import Tasks from '../components/Tasks'
-import { Text, View } from '../components/Themed';
 import firebase from 'firebase';
 import db from '../db/firestore';
 
 export default function UserScreen() {
-/*firebase.firestore
-  const storeFoodData = (userId, cal) => {
-    //const userId: "123456@gmail.com"
-    //const cal: 100
-    firebase
-      .database()
-      .ref('users/' + userId)
-      .set({
-        FoodCalories: cal,
-      });
+//  const [items, setItems] = useState<Object[]>()
+  const [items, setItems] = useState('')
 
-      //return null
+  useEffect(() => {
+      db.collection('users')
+        .get()
+        .then(result => result.docs)
+        .then(docs => docs.map(doc=>({
+          id: doc.id,
+          barCodeNum: doc.data().barCodeNum,
+          createdAt:doc.data().createdAt,
+          foodCalories: doc.data().foodCalories,
+          foodName: doc.data().foodName
+        })))
+        .then(items => setItems(items))
+    }, [])
+  
 
-  }
-
-  const submitData = () => {
-    storeFoodData("123456@gmail", 100)
-  }
-*/
-
+  const reloadData = () => {
+      db.collection('users')
+        .get()
+        .then(result => result.docs)
+        .then(docs => docs.map(doc=>({
+          id: doc.id,
+          barCodeNum: doc.data().barCodeNum,
+          createdAt:doc.data().createdAt,
+          foodCalories: doc.data().foodCalories,
+          foodName: doc.data().foodName
+        })))
+        .then(items => setItems(items))
+    }
 
   return (
+//    <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
     <View style={styles.container}>
-      <Text style={styles.title}>User Screen</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Tasks/>
+      <TouchableOpacity style={styles.reloadPage} onPress={reloadData}>
+                <Text style={styles.reloadText}>Reload</Text>
+      </TouchableOpacity>
+    <View style={styles.titlePos}>
+      <Text style = {styles.title}>Scanned List</Text>
+    </View>
+
+
+      <Text>{items.foodName}</Text>
+      <Text></Text>
+
 
     </View> 
   );
@@ -44,13 +63,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  foodList: {
+    flexDirection: 'row',
+  },
+  reloadPage: {
+    position: 'absolute',
+    top: 20,
+    right: 20,
+    width: 50,
+    height: 50,
+},
+  reloadText: {
+    fontWeight: 'bold',
+    color: 'red'
+},
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  titlePos: {
+    position: 'absolute',
+    top: 50,
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
   },
+
 });
