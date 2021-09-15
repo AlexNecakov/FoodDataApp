f288e97c10328aeeac843e449e294f4fdc141cc2
kevinlimBU <kevinlim212@gmail.com>
2021-09-15 01:17:26 -0400

    upload to firebase

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, Button } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { RootStackScreenProps } from '../types';
+import db from '../db/firestore'
+
 
 export default function ModalScreen({ route, navigation }: RootStackScreenProps<'Modal'>) {
     const barCode: string = route.params.upcCode;
@@ -33,16 +35,29 @@ export default function ModalScreen({ route, navigation }: RootStackScreenProps<
     }
     useEffect(() => {
         getFoodProfilefromFDA(barCode);
+        
     });
+
+    const uploadData = () => {
+        db.collection('users').add({
+            foodName: foodDescription,
+            barCodeNum: barCode,
+            foodCalories: null,
+            createdAt: new Date()
+        }).catch(err => console.log(err))
+    }
+
 
     return (
 
+        
         <View style={styles.container}>
             <Text style={styles.title}> {foodDescription}</Text>
             <Text style={styles.title}> {barCode}</Text>
             <Text style={styles.title}> {totalHits}</Text>
             <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
 
+            <Button title="upload" onPress={uploadData} />
             {/* Use a light status bar on iOS to account for the black space above the modal */}
             <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
         </View>
