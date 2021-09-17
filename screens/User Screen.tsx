efae8f654b0abb8858a6d22e6213374dd28adc72
kevinlimBU <kevinlim212@gmail.com>
2021-09-17 02:49:35 -0400

    update user screen

--- screens/User Screen.tsx
+++ screens/User Screen.tsx
@@ -1,58 +1,99 @@
 //import * as React from 'react';
-import React, {FC, useState, useEffect} from 'react';
+import React, {FC, useState, useEffect, SetStateAction} from 'react';
 import { Text, View, StyleSheet, Button, TouchableOpacity} from 'react-native';
 import Tasks from '../components/Tasks'
 import firebase from 'firebase';
 import db from '../db/firestore';
+import Breakfast from '../components/Breakfast';
+import Lunch from '../components/Lunch';
+import Dinner from '../components/Dinner';
 
 export default function UserScreen() {
-//  const [items, setItems] = useState<Object[]>()
-  const [items, setItems] = useState('')
+  const [items, setItems] = useState<any>()
+//  const [items, setItems] = useState("");
+let [userView, setUserView] = useState('');
+let [reload, setReload] = useState(false);
+let [Foodcalories, setFoodcalories] = useState('');
+let [Foodname, setFoodname] = useState('');
+let [Createdat, setCreatedat] = useState('');
 
-  useEffect(() => {
-      db.collection('users')
-        .get()
-        .then(result => result.docs)
-        .then(docs => docs.map(doc=>({
-          id: doc.id,
-          barCodeNum: doc.data().barCodeNum,
-          createdAt:doc.data().createdAt,
-          foodCalories: doc.data().foodCalories,
-          foodName: doc.data().foodName
-        })))
-        .then(items => setItems(items))
-    }, [])
-  
+type foodList = {
+  id: string;
+  barCodeNum: string;
+  createdAt: string;
+  foodCalories: string;
+  foodName: string;
+}
 
-  const reloadData = () => {
-      db.collection('users')
-        .get()
-        .then(result => result.docs)
-        .then(docs => docs.map(doc=>({
-          id: doc.id,
-          barCodeNum: doc.data().barCodeNum,
-          createdAt:doc.data().createdAt,
-          foodCalories: doc.data().foodCalories,
-          foodName: doc.data().foodName
-        })))
-        .then(items => setItems(items))
+    useEffect(()=>{
+      reload?
+      db.collection('users').onSnapshot({
+        next: querySnapshot => {
+          const items = querySnapshot.docs.map(docSnapshot => ({
+          id: docSnapshot.id,
+          barCodeNum: docSnapshot.data().barCodeNum,
+          createdAt: docSnapshot.data().createdAt,
+          foodCalories: docSnapshot.data().foodCalories,
+          foodName: docSnapshot.data().foodName
+          }))
+          setItems(items)
+        },
+        error:(error)=>console.log(error)
+      })
+      :console.log('No Change')
+      
+      setReload(false)
+
+    })
+
+    const loadList = () => {
+      var len = Object.keys(items).length
+      var text = "";
+      //setReload(true)
+      for(let i =0; i < len; i++){
+        //setUserView(JSON.stringify(items[i].foodCalories)+' Calories'+'\n'
+        //+JSON.stringify(items[i].foodName))
+        text = JSON.stringify(items[i].foodCalories)+' Calories'+'\n'
+        +JSON.stringify(items[i].foodName)+'\n\n'+text;
+      }
+      setUserView(text)
     }
+
+    const loadAgain = ()=>{
+      setReload(true)
+      var len = Object.keys(items).length
+      if(len>0){
+        loadList()
+      }
+    }
+
 
   return (
 //    <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
     <View style={styles.container}>
-      <TouchableOpacity style={styles.reloadPage} onPress={reloadData}>
-                <Text style={styles.reloadText}>Reload</Text>
-      </TouchableOpacity>
+    <View style = {styles.breakfast}>
+      <Breakfast/>
+    </View>
+    <View style = {styles.lunch}>
+      <Lunch/>
+    </View>
+    <View style = {styles.dinner}>
+      <Dinner/>
+    </View>
     <View style={styles.titlePos}>
       <Text style = {styles.title}>Scanned List</Text>
     </View>
+    <TouchableOpacity style={styles.reloadPage} onPress={loadAgain}>
+      <Text style={styles.reloadText}>Reload</Text>
+    </TouchableOpacity>
+    <View style = {styles.barList}> 
 
-
-      <Text>{items.foodName}</Text>
-      <Text></Text>
-
-
+      
+        <Text style={styles.foodCal}>{userView? userView :"No Item"}</Text>
+      
+      
+         
+    </View>
     </View> 
   );
 }
@@ -68,7 +109,7 @@ const styles = StyleSheet.create({
   },
   reloadPage: {
     position: 'absolute',
-    top: 20,
+    top: 55,
     right: 20,
     width: 50,
     height: 50,
@@ -84,6 +125,28 @@ const styles = StyleSheet.create({
   titlePos: {
     position: 'absolute',
     top: 50,
+  },
+  foodCal:{
+    fontWeight: 'bold',
+  },
+  breakfast:{
+    position: 'absolute',
+    top: 15,
+    left: 20,
+  },
+  lunch:{
+    position: 'absolute',
+    top: 15,
+    left: 150,
+  },
+  dinner:{
+    position: 'absolute',
+    top: 15,
+    left: 300,
+  },
+  barList:{
+    position: 'absolute',
+    top: 100,
   },
   separator: {
     marginVertical: 30,
